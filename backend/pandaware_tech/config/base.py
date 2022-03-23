d85699198e1bef9eca3747b4745eef7f8ca3ab91
Israel Abraham <israelvictory87@gmail.com>
2022-03-23 13:30:31 +0100

    Deleted settings.py file, and created 3 config components

--- backend/pandaware_tech/config/base.py
+++ backend/pandaware_tech/config/base.py
@@ -70,17 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pandaware_tech.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
